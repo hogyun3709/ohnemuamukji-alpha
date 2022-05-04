adb6e8cd870bb63881d5403dd7c75461cf05b3ae
hogyun3709 <hogyun3709@gmail.com>
2022-05-04 14:45:29 +0900

    Setup: refactor using alias in vite with typescript

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,14 @@
 /// <reference types="vitest" />
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import * as path from 'path'
 
-const path = require('path');
 
 // https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src')
+      '@' : path.resolve(__dirname, './src')
     }
   },
   plugins: [vue()],
