4cdf9515f9ac97912e6c6afd48233cb74e5d5413
hogyun3709 <hogyun3709@gmail.com>
2022-05-04 12:41:43 +0900

    Setup: configure testing env and alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
+/// <reference types="vitest" />
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+const path = require('path');
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  },
+  plugins: [vue()],
+  test: {
+    globals: true,
+    environment: 'jsdom'
+  }
 })
+
