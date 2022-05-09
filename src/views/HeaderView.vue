f4e49974edbb2c09051dc78e0e724ea8fda4d2e0
hogyun3709 <hogyun3709@gmail.com>
2022-05-09 14:26:11 +0900

    Setup(route): build initial project structure

---
+++ src/views/HeaderView.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <h1>헤더 뷰 입니다.</h1>	
+</template>
---
+++ src/views/RouletteView.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <h1>룰렛 뷰 입니다.</h1>	
+</template>
