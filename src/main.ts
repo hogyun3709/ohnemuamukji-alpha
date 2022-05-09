501a43a828ef55459ee4beeac1c5b94470b959f5
hogyun3709 <hogyun3709@gmail.com>
2022-05-10 00:02:52 +0900

    Markup(roulette): create roulette board section

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,29 @@ import router from "./router";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
-import { faBars, faCamera } from "@fortawesome/free-solid-svg-icons";
-import { faUser } from "@fortawesome/free-regular-svg-icons";
+import {
+  faBars,
+  faCamera,
+  faCaretDown,
+  faEraser,
+} from "@fortawesome/free-solid-svg-icons";
+import {
+  faUser,
+  faMinusSquare,
+  faPlusSquare,
+} from "@fortawesome/free-regular-svg-icons";
 
 import "./index.css";
 
-library.add(faBars, faUser, faCamera);
+library.add(
+  faBars,
+  faUser,
+  faCamera,
+  faCaretDown,
+  faMinusSquare,
+  faPlusSquare,
+  faEraser
+);
 
 createApp(App)
   .use(router)
--- src/views/RouletteView.vue
+++ src/views/RouletteView.vue
@@ -1,7 +1,22 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import RouletteBoard from "@/components/RouletteBoard.vue";
+</script>
 
 <template>
-  <main>
-    <h1 data-test="roulette-view-root">룰렛 뷰 입니다.</h1>
+  <main class="flex flex-col justify-center" data-test="roulette-view-root">
+    <!--result-->
+    <div class="my-4 mx-auto" data-test="option-result"></div>
+    <!--indicator-->
+    <div class="mx-auto" data-test="option-indicator">
+      <font-awesome-icon class="w-12 h-12" :icon="['fa', 'caret-down']" />
+    </div>
+    <!--roulette-->
+    <RouletteBoard data-test="roulette-board" />
+    <!--option controller-->
+    <div class="flex justify-around my-10" data-test="option-controller">
+      <font-awesome-icon class="w-8 h-8" :icon="['far', 'minus-square']" />
+      <font-awesome-icon class="w-8 h-8" :icon="['fa', 'eraser']" />
+      <font-awesome-icon class="w-8 h-8" :icon="['far', 'plus-square']" />
+    </div>
   </main>
 </template>
