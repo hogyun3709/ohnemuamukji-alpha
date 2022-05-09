bb942b88b160fa55e4754d62bb34d5ef1c6e6d87
hogyun3709 <hogyun3709@gmail.com>
2022-05-09 19:02:36 +0900

    Add(fontawesome): add fontawesome icon packages

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,17 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+import { faBars, faCamera } from "@fortawesome/free-solid-svg-icons";
+import { faUser } from "@fortawesome/free-regular-svg-icons";
+
 import "./index.css";
 
-createApp(App).use(router).mount("#app");
+library.add(faBars, faUser, faCamera);
+
+createApp(App)
+  .use(router)
+  .component("font-awesome-icon", FontAwesomeIcon)
+  .mount("#app");
