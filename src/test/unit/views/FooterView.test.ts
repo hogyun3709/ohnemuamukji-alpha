a2042241214727c5bdcf30ad8fefe1df9ca8145f
hogyun3709 <hogyun3709@gmail.com>
2022-05-23 16:08:04 +0900

    test: resolve un-imported plugins - vue router in footer view

--- src/test/unit/views/FooterView.test.ts
+++ src/test/unit/views/FooterView.test.ts
@@ -1,9 +1,20 @@
 import { mount } from "@vue/test-utils";
+import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
+import { routes } from '@/router'
 
 import FooterView from "@/views/FooterView.vue";
 
+const router = createRouter({
+  history: createWebHistory(),
+  routes: routes
+})
+
 describe("Test case for FooterView file", () => {
-  const wrapper = mount(FooterView);
+  const wrapper = mount(FooterView, {
+    global: {
+      plugins: [router]
+    }
+  });
 
   it("renders FooterView root element", () => {
     expect(
