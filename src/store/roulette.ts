23cae20c24887f8f65fca48379575ed46de39834
hogyun3709 <hogyun3709@gmail.com>
2022-05-23 13:27:26 +0900

    lint: re-run lint script

--- src/store/roulette.ts
+++ src/store/roulette.ts
@@ -2,109 +2,108 @@ import { defineStore } from "pinia";
 import { Roulette } from "@/types/roulette";
 
 const optionsValidation: Roulette = {
-	minQuantity: 2,
-	maxQuantity: 4,
-	boardList: [
-		"https://ssl.pstatic.net/sstatic/keypage/outside/mobilegame/circle/im/im/bg_circle2.png",
-		"https://ssl.pstatic.net/sstatic/keypage/outside/mobilegame/circle/im/im/bg_circle3.png",
-		"https://ssl.pstatic.net/sstatic/keypage/outside/mobilegame/circle/im/im/bg_circle4.png",
-		"https://ssl.pstatic.net/sstatic/keypage/outside/mobilegame/circle/im/im/bg_circle5.png",
-		"https://ssl.pstatic.net/sstatic/keypage/outside/mobilegame/circle/im/im/bg_circle6.png",
-		"https://ssl.pstatic.net/sstatic/keypage/outside/mobilegame/circle/im/im/bg_circle7.png",
-		"https://ssl.pstatic.net/sstatic/keypage/outside/mobilegame/circle/im/im/bg_circle8.png",
-	],
-	optionList: [],
-	optionFirst: "",
-	optionSecond: "",
-	optionThird: "",
-	optionForth: "",
-	optionFirstDone: false,
-	optionSecondDone: false,
-	optionThirdDone: false,
-	optionForthDone: false,
+  minQuantity: 2,
+  maxQuantity: 4,
+  boardList: [
+    "https://ssl.pstatic.net/sstatic/keypage/outside/mobilegame/circle/im/im/bg_circle2.png",
+    "https://ssl.pstatic.net/sstatic/keypage/outside/mobilegame/circle/im/im/bg_circle3.png",
+    "https://ssl.pstatic.net/sstatic/keypage/outside/mobilegame/circle/im/im/bg_circle4.png",
+  ],
+  optionList: [],
+  optionFirst: "",
+  optionSecond: "",
+  optionThird: "",
+  optionForth: "",
+  optionFirstDone: false,
+  optionSecondDone: false,
+  optionThirdDone: false,
+  optionForthDone: false,
 };
 
 export const useRouletteStore = defineStore("roulette", {
-	state: () => ({
-		optionCurrentQuantity: optionsValidation.minQuantity,
-		optionMinQuantity: optionsValidation.minQuantity,
-		optionMaxQuantity: optionsValidation.maxQuantity,
-		optionBoardList: optionsValidation.boardList,
-		optionTotalList: optionsValidation.optionList,
-		optionFirstText: optionsValidation.optionFirst,
-		optionSecondText: optionsValidation.optionSecond,
-		optionThirdText: optionsValidation.optionThird,
-		optionForthText: optionsValidation.optionForth,
-		optionFirstCompleted: optionsValidation.optionFirstDone,
-		optionSecondCompleted: optionsValidation.optionSecondDone,
-		optionThirdCompleted: optionsValidation.optionThirdDone,
-		optionForthCompleted: optionsValidation.optionForthDone,
-	}),
+  state: () => ({
+    optionCurrentQuantity: optionsValidation.minQuantity,
+    optionMinQuantity: optionsValidation.minQuantity,
+    optionMaxQuantity: optionsValidation.maxQuantity,
+    optionBoardList: optionsValidation.boardList,
+    optionTotalList: optionsValidation.optionList,
+    optionFirstText: optionsValidation.optionFirst,
+    optionSecondText: optionsValidation.optionSecond,
+    optionThirdText: optionsValidation.optionThird,
+    optionForthText: optionsValidation.optionForth,
+    optionFirstCompleted: optionsValidation.optionFirstDone,
+    optionSecondCompleted: optionsValidation.optionSecondDone,
+    optionThirdCompleted: optionsValidation.optionThirdDone,
+    optionForthCompleted: optionsValidation.optionForthDone,
+  }),
 
-	getters: {
-		getRouletteBoard(state): string {
-			return state.optionBoardList[
-				state.optionCurrentQuantity - state.optionMinQuantity
-			];
-		},
-	},
+  getters: {
+    getRouletteBoard(state): string {
+      return state.optionBoardList[
+        state.optionCurrentQuantity - state.optionMinQuantity
+      ];
+    },
+    getIsOptionFirstCompleted(state): boolean {
+      return state.optionFirstCompleted;
+    },
+  },
 
-	actions: {
-		changeOptionQuantity(value: number) {
-			this.optionCurrentQuantity += value;
-			if (this.optionCurrentQuantity < this.optionMinQuantity) {
-				this.optionCurrentQuantity = this.optionMinQuantity;
-			}
-			if (this.optionCurrentQuantity > this.optionMaxQuantity) {
-				this.optionCurrentQuantity = this.optionMaxQuantity;
-			}
-		},
-		setFirstOption(e: Event) {
-			this.optionFirstText = (e.target as HTMLInputElement).value;
-			this.optionTotalList.push(this.optionFirstText);
-		},
-		setSecondOption(e: Event) {
-			this.optionSecondText = (e.target as HTMLInputElement).value;
-		},
-		setThirdOption(e: Event) {
-			this.optionThirdText = (e.target as HTMLInputElement).value;
-		},
-		setForthOption(e: Event) {
-			this.optionForthText = (e.target as HTMLInputElement).value;
-		},
-		addFirstOptionToList() {
-			this.optionTotalList[0] = this.optionFirstText;
-			this.optionFirstCompleted = true;
-		},
-		addSecondOptionToList() {
-			this.optionTotalList[1] = this.optionSecondText;
-			this.optionSecondCompleted = true;
-		},
-		addThridOptionToList() {
-			this.optionTotalList[2] = this.optionThirdText;
-			this.optionThirdCompleted = true;
-		},
-		addForthOptionToList() {
-			this.optionTotalList[3] = this.optionForthText;
-			this.optionForthCompleted = true;
-		},
-		editFirstOption() {
-			this.optionFirstCompleted = false;
-		},
-		editSecondOption() {
-			this.optionSecondCompleted = false;
-		},
-		editThirdOption() {
-			this.optionThirdCompleted = false;
-		},
-		editForthOption() {
-			this.optionForthCompleted = false;
-		},
-		clearOptionTextTotal() {
-			this.optionFirstText = "";
-			this.optionSecondText = "";
-			this.optionThirdText = "";
-			this.optionForthText = "";
-		},
-	},
+  actions: {
+    changeOptionQuantity(value: number) {
+      this.optionCurrentQuantity += value;
+      if (this.optionCurrentQuantity < this.optionMinQuantity) {
+        this.optionCurrentQuantity = this.optionMinQuantity;
+      }
+      if (this.optionCurrentQuantity > this.optionMaxQuantity) {
+        this.optionCurrentQuantity = this.optionMaxQuantity;
+      }
+    },
+    setFirstOption(e: Event) {
+      this.optionFirstText = (e.target as HTMLInputElement).value;
+      this.optionTotalList.push(this.optionFirstText);
+    },
+    setSecondOption(e: Event) {
+      this.optionSecondText = (e.target as HTMLInputElement).value;
+    },
+    setThirdOption(e: Event) {
+      this.optionThirdText = (e.target as HTMLInputElement).value;
+    },
+    setForthOption(e: Event) {
+      this.optionForthText = (e.target as HTMLInputElement).value;
+    },
+    addFirstOptionToList() {
+      this.optionTotalList[0] = this.optionFirstText;
+      this.optionFirstCompleted = true;
+    },
+    addSecondOptionToList() {
+      this.optionTotalList[1] = this.optionSecondText;
+      this.optionSecondCompleted = true;
+    },
+    addThridOptionToList() {
+      this.optionTotalList[2] = this.optionThirdText;
+      this.optionThirdCompleted = true;
+    },
+    addForthOptionToList() {
+      this.optionTotalList[3] = this.optionForthText;
+      this.optionForthCompleted = true;
+    },
+    editFirstOption() {
+      this.optionFirstCompleted = false;
+    },
+    editSecondOption() {
+      this.optionSecondCompleted = false;
+    },
+    editThirdOption() {
+      this.optionThirdCompleted = false;
+    },
+    editForthOption() {
+      this.optionForthCompleted = false;
+    },
+    clearOptionTextTotal() {
+      this.optionFirstText = "";
+      this.optionSecondText = "";
+      this.optionThirdText = "";
+      this.optionForthText = "";
+    },
+  },
 });
--- src/test/unit/views/RouletteView.test.ts
+++ src/test/unit/views/RouletteView.test.ts
@@ -1,18 +1,18 @@
 import { mount } from "@vue/test-utils";
-import { createTestingPinia } from '@pinia/testing';
+import { createTestingPinia } from "@pinia/testing";
 
 import RouletterView from "@/views/RouletteView.vue";
 
 describe("Test case for RouletteView file", () => {
-	const wrapper = mount(RouletterView, {
-		global: {
-			plugins: [createTestingPinia()],
-		}
-	});
+  const wrapper = mount(RouletterView, {
+    global: {
+      plugins: [createTestingPinia()],
+    },
+  });
 
-	it("renders FooterView root element", () => {
-		expect(
-			wrapper.find('[data-test="roulette-view-root"]').exists()
-		).toBeTruthy();
-	});
+  it("renders FooterView root element", () => {
+    expect(
+      wrapper.find('[data-test="roulette-view-root"]').exists()
+    ).toBeTruthy();
+  });
 });
