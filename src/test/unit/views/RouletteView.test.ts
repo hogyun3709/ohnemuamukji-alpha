4952a5c29c5b98aa94b7f4f19f8475bbe4639e38
hogyun3709 <hogyun3709@gmail.com>
2022-05-23 13:25:43 +0900

    test: resolve missing pinia in roulette view component

--- src/test/unit/views/RouletteView.test.ts
+++ src/test/unit/views/RouletteView.test.ts
@@ -1,13 +1,18 @@
 import { mount } from "@vue/test-utils";
+import { createTestingPinia } from '@pinia/testing';
 
 import RouletterView from "@/views/RouletteView.vue";
 
 describe("Test case for RouletteView file", () => {
-  const wrapper = mount(RouletterView);
+	const wrapper = mount(RouletterView, {
+		global: {
+			plugins: [createTestingPinia()],
+		}
+	});
 
-  it("renders FooterView root element", () => {
-    expect(
-      wrapper.find('[data-test="roulette-view-root"]').exists()
-    ).toBeTruthy();
-  });
+	it("renders FooterView root element", () => {
+		expect(
+			wrapper.find('[data-test="roulette-view-root"]').exists()
+		).toBeTruthy();
+	});
 });
